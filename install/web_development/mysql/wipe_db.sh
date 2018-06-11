524c31aaf402a7c7a1a5d1148f30bf596d6da424
Nicolas Bages <spyesx@users.noreply.github.com>
2018-06-11 19:38:36 +0800

    Update wipe_db.sh

--- install/web_development/mysql/wipe_db.sh
+++ install/web_development/mysql/wipe_db.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Wipe a database
+# Backup & Wipe a database
 # sudo sh wipe_db.sh dbname
 
 DBUSER="xxxxxxxxxxxx"
 DBPASS="yyyyyyyyyyyyyy"
-
 DBNAME="$1"
 
 # Require the database argument.
@@ -14,5 +13,8 @@ DBNAME="$1"
   exit 1;
 }
 
-# Drop the given database with mysql
+echo "Create a backup of $DBNAME"
+mysqldump -u$DBUSER -p$DBPASS $DBNAME > "$DBNAME.sql"
+
+echo "Drop $DBNAME"
 mysql -u$DBUSER -p$DBPASS -Nse 'show tables' $DBNAME | while read table; do mysql -u$DBUSER -p$DBPASS -e "drop table $table" $DBNAME; done
